4d588ccdbac6cdac41bcc98b7bbf0a6ceaeb6ff6
Simeon Leifer <simeon@droolingcat.com>
2018-09-15 14:49:27 -0700

    Initial commit with empty macOS framework and test app

---
+++ MessageQueue/MessageQueue.h
@@ -0,0 +1,19 @@
+//
+//  MessageQueue.h
+//  MessageQueue
+//
+//  Created by Simeon Leifer on 9/15/18.
+//  Copyright © 2018 droolongcat.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MessageQueue.
+FOUNDATION_EXPORT double MessageQueueVersionNumber;
+
+//! Project version string for MessageQueue.
+FOUNDATION_EXPORT const unsigned char MessageQueueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MessageQueue/PublicHeader.h>
+
+
---
+++ MessageQueueTryout/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MessageQueueTryout
+//
+//  Created by Simeon Leifer on 9/15/18.
+//  Copyright © 2018 droolongcat.com. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
