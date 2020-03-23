bdd560e7b3ae307427d473e56f49f236863f41d7
Simeon Leifer <simeon@droolingcat.com>
2020-03-23 07:10:57 -0700

    add a dynamic framwork target

--- Package.swift
+++ Package.swift
@@ -9,7 +9,10 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "MessageQueue",
-            targets: ["MessageQueue"])
+            targets: ["MessageQueue"]),
+    .library(
+        name: "MessageQueueDynamic", type: .dynamic,
+        targets: ["MessageQueue"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
